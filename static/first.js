0738c466d20240f40a884eb2a77f5c0e749d931f
Davit Mkrtchyan <david.mkrtchyan00@gmail.com>
2022-11-26 19:52:12 +0400

    fix/image button label clickable

--- static/first.js
+++ static/first.js
@@ -1,10 +1,20 @@
 const buttonFemale = document.querySelector('input.female');
 const buttonMale = document.querySelector('input.male');
+const buttonFemaleText = document.querySelector('div.textf');
+const buttonMaleText = document.querySelector('div.textm');
 
 buttonFemale.addEventListener('click', () => {
   window.location.href = window.location.href + 'her';
 });
 
 buttonMale.addEventListener('click', () => {
+  window.location.href = window.location.href + 'him';
+});
+
+buttonFemaleText.addEventListener('click', () => {
+  window.location.href = window.location.href + 'her';
+});
+
+buttonMaleText.addEventListener('click', () => {
   window.location.href = window.location.href + 'him';
 });
